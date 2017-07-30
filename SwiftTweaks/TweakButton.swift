7d0d0835134ff0b1480a8b70e94ef743b2f6c9fb
Jarek Pendowski <jarek@pendowski.com>
2017-07-30 21:54:10 +0200

    Improved TweakButton using inits.

--- SwiftTweaks/TweakButton.swift
+++ SwiftTweaks/TweakButton.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class TweakButton: UIButton {
+final class TweakButton: UIButton {
 	
 	typealias ControlState = UInt
 	
-	private var borderColors: [ControlState: UIColor] = [:]
+	@objc private var borderColors: [ControlState: UIColor] = [:]
+	
 	override var isHighlighted: Bool {
 		didSet { updateState() }
 	}
@@ -31,12 +32,39 @@ class TweakButton: UIButton {
 	
 	func setBorderColor(_ color: UIColor, for state: UIControlState = .normal) {
 		borderColors[state.rawValue] = color
+		updateState()
 	}
 	
-	override func didMoveToWindow() {
-		super.didMoveToWindow()
+	func borderColor(for state: UIControlState) -> UIColor? {
+		return borderColors[state.rawValue]
+	}
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
 		
 		updateState()
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		
+		// NSCoder doesn't get CGFloats, and although is a Double _right now_, we could use a little trick to avoid casing
+		borderWidth = aDecoder.decodeCGVector(forKey: #keyPath(borderWidth)).dx
+		cornerRadius = aDecoder.decodeCGVector(forKey: #keyPath(cornerRadius)).dx
+		
+		if let colorsString = aDecoder.decodeObject(forKey: #keyPath(borderColors)) as? String {
+			borderColors = type(of: self).decodeBorderColors(from: colorsString)
+		}
+		
+		updateState()
+	}
+	
+	override func encode(with aCoder: NSCoder) {
+		super.encode(with: aCoder)
+		
+		aCoder.encode(CGVector(dx: borderWidth, dy: 0), forKey: #keyPath(borderWidth))
+		aCoder.encode(CGVector(dx: cornerRadius, dy: 0), forKey: #keyPath(cornerRadius))
+		aCoder.encode(type(of: self).encodeBorderColors(from: borderColors), forKey: #keyPath(borderColors))
 	}
 	
 	// MARK: - Private
@@ -55,5 +83,28 @@ class TweakButton: UIButton {
 		size.height += borderWidth * 2
 		
 		return size
+	}
+	
+	private static func encodeBorderColors(from colors: [ControlState: UIColor]) -> String {
+		var encodedString = ""
+		for (state, color) in colors {
+			encodedString += "\(state):\(color.hexString)|"
+		}
+		return encodedString
+	}
+	
+	private static func decodeBorderColors(from string: String) -> [ControlState: UIColor] {
+		var colors: [ControlState: UIColor] = [:]
+		let pairs = string.components(separatedBy: "|")
+		pairs.forEach {
+			let pair = $0.components(separatedBy: ":")
+			guard pair.count == 2,
+				let state = ControlState(pair[0]),
+				let color = UIColor.colorWithHexString(pair[1]) else{
+				return
+			}
+			colors[state] = color
+		}
+		return colors
 	}
 }
