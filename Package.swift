75aca88a546a8ac2899f82fc710dca51020ff18f
Kim Phifer <kim@rho.co>
2020-04-24 15:55:11 -0500

    ios only, swift tools

--- Package.swift
+++ Package.swift
@@ -1,13 +1,13 @@
+// swift-tools-version:5.1
 //
 //  Package.swift
+//
+//
 //
 import PackageDescription
 
 let package = Package(name: "SwiftTweaks",
-platforms: [.macOS(.v10_12),
-            .iOS(.v10),
-            .tvOS(.v10),
-            .watchOS(.v3)],
+platforms: [.iOS(.v10)],
 products: [.library(name: "SwiftTweaks",
                     targets: ["SwiftTweaks"])],
 targets: [.target(name: "SwiftTweaks",
